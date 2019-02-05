9392b5ec3064248d202334e7a8e024270a900286
Shubham Jain <45535803+shubhamjain839@users.noreply.github.com>
2019-02-06 00:42:52 +0530

    Add files via upload

---
+++ doubleyv2.c
@@ -0,0 +1,268 @@
+/*		copyright	Shubham Jain
+			DCSA MCA E
+				*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+struct dnode
+{
+	int info;
+	struct dnode *prev,*next;
+};
+void Trav(struct dnode *head,struct dnode *tail)
+{
+	int i=1;
+	if(head==NULL)
+		printf("\nList is Empty !");
+	else
+	{	
+		while(head!=NULL)
+		{
+			printf("\n%dth Element is :%d",i,head->info);
+			i++;
+			head=head->next;
+		}
+	}
+}
+void InsBeg(struct dnode **head,struct dnode **tail,int info)
+{
+	struct dnode *temp;
+	temp=(struct dnode*)malloc(sizeof(struct dnode));
+	temp->info=info;
+	if((*head)==NULL && (*tail)==NULL)
+	{
+		temp->next=NULL;
+		temp->prev=NULL;
+		*head=temp;
+		*tail=temp;
+	}
+	else
+	{
+		temp->prev=NULL;
+		temp->next=*head;
+		(*head)->prev=temp;
+		*head=temp;
+	}
+}
+void InsMid(struct dnode **head,struct dnode **tail,int info,int loc)
+{
+	int i,flag=0;
+	struct dnode *temp,*cp=(*head);
+	temp=(struct dnode*)malloc(sizeof(struct dnode));
+	temp->info=info;
+	for	(i=0;i<loc;i++)
+	{
+		if(cp==NULL)
+		{
+			printf("\nEnd of List Reached !");
+			flag=1;
+			break;
+		}
+		else
+		{
+			cp=cp->next;
+		}
+	}
+	if(flag!=1)
+	{
+		if (cp->next==NULL)
+		{
+			temp->prev=cp;
+			temp->next=NULL;
+			cp->next=temp;
+			(*tail)=temp;
+		}
+		else
+		{
+			temp->prev=cp;
+			temp->next=cp->next;
+			cp->next->prev=temp;
+			cp->next=temp;
+		}
+	}
+}
+void InsEnd(struct dnode **head,struct dnode **tail,int info)
+{
+	struct dnode *temp;
+	temp=(struct dnode*)malloc(sizeof(struct dnode));
+	temp->info=info;
+	if((*head)==NULL && (*tail)==NULL)
+	{
+		temp->next=NULL;
+		temp->prev=NULL;
+		*head=temp;
+		*tail=temp;
+	}
+	else
+	{
+		temp->prev=*tail;
+		temp->next=NULL;
+		(*tail)->prev=temp;
+		*tail=temp;
+	}
+}
+void DelBeg(struct dnode **head,struct dnode **tail)
+{
+	int info;
+	if((*head)==NULL && (*tail)==NULL)
+	{
+		printf("List is Empty !");
+	}
+	else
+	{
+		if((*head)!=(*tail))
+		{
+			info=(*head)->info;
+			(*head)=(*head)->next;
+			(*head)->prev=NULL;
+		}
+		else
+		{
+			info=(*head)->info;
+			(*head)=NULL;
+			(*tail)=NULL;
+		}
+		printf("\nDeleted Value is :%d",info);
+	}
+}
+void DelMid(struct dnode **head,struct dnode **tail,int loc)
+{
+	int info,i;
+	struct dnode *cp=(*head);
+	if((*head)==NULL && (*tail)==NULL)
+	{
+		printf("List is Empty !");
+	}
+	else
+	{
+		if((*head)!=(*tail))
+		{
+			for (i=0;i<loc;i++)
+			{
+				if(cp==NULL)
+				{
+					printf("\nEnd of List reached !");
+					break;
+				}
+				cp=cp->next;
+			}	
+				info=cp->info;
+				cp->prev->next=cp->next;	
+				cp->next->prev=cp->prev;
+				printf("\nElement Deleted :%d",info);
+		}
+		else
+		{
+			info=(*head)->info;
+			(*head)=NULL;
+			(*tail)=NULL;
+		}
+		printf("\nDeleted Value is :%d",info);
+	}
+}
+void DelEnd(struct dnode **head,struct dnode **tail)
+{
+	int info;
+	if((*head)==NULL && (*tail)==NULL)
+	{
+		printf("List is Empty !");
+	}
+	else
+	{
+		if((*head)!=(*tail))
+		{
+			info=(*tail)->info;
+			(*tail)=(*tail)->prev;
+			(*tail)->next=NULL;
+		}
+		else
+		{
+			info=(*head)->info;
+			(*head)=NULL;
+			(*tail)=NULL;
+		}
+		printf("\nDeleted Value is :%d",info);
+	}	
+}
+int main()
+{
+	struct dnode *head=NULL,*tail=NULL;
+	int ch,ch1,loc,info;
+	while (1)
+	{
+		printf("\n\n\n============+WELCOME+===========\n\n");
+		printf("1.Traverse List\n2.Insert Element\n3.Delete Element\n4.Exit\n*Enter your choice :");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				{	
+				Trav(head,tail);
+				}break;
+			case 2:
+				{
+					printf("\n============+Insert Menu+=========\n\n");
+					printf("1.Insert at Beginning\n2.Insert at Mid Loc\n3.Insert at End\n4.Back\n*Enter your choice :");
+					scanf("%d",&ch1);
+					switch(ch1)
+					{
+						case 1:
+							{
+								printf("Enter Element :");
+								scanf("%d",&info);
+								InsBeg(&head,&tail,info);
+							}break;
+						case 2:
+							{
+								printf("Enter Element and Position :");
+								scanf("%d%d",&info,&loc);
+								InsMid(&head,&tail,info,loc);
+							}break;
+						case 3:
+							{
+								printf("Enter Element :");
+								scanf("%d",&info);
+								InsEnd(&head,&tail,info);
+							}break;
+						case 4:
+							break;
+						default :
+							printf("Not a valid choice !\n");
+					}
+				}break;
+			case 3:
+				{
+					printf("\n============+Delete Menu+=========\n\n");
+					printf("1.Delete at Beginning\n2.Delete at Mid Loc\n3.Delete at End\n4.Back\n*Enter your choice :");
+					scanf("%d",&ch1);
+					switch(ch1)
+					{
+						case 1:
+							{
+								DelBeg(&head,&tail);
+							}break;
+						case 2:
+							{
+								printf("Enter Position :");
+								scanf("%d",&loc);
+								DelMid(&head,&tail,loc);
+							}break;
+						case 3:
+							{
+								DelEnd(&head,&tail);
+							}break;
+						case 4:
+							break;
+						default :
+							printf("Not a valid choice !\n");
+					}
+				}break;
+			case 4:
+				exit(0);
+			default :
+					printf("Enter valid choice !");
+		}
+	}
+	return 0;
+}
